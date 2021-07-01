6aff506a6e34febf87eb6227a692a51a61329f7e
Vlad Huliaiev <gulyaev4420@gmail.com>
2021-07-01 22:36:03 +0300

    change add timer, delete react file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Timer from "./Timer/Timer.js";
 import TimerRx from "./TimerRx/TimerRx.js";
 
 function App() {
   return (
     <div>
-      <TimerRx startTime={10000} />
-      
+      <TimerRx startTime={3590000} />
     </div>
   );
 }
